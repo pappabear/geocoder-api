126bfafc67705ca1c028bc964517057864fb3548
Chip Irek <chip.irek@gmail.com>
2018-05-20 15:59:05 -0400

    Added body parser explicitly hoping that helps on heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express'),
   port = process.env.PORT || 3000,
   bodyParser = require('body-parser')
 
-app.use(bodyParser.urlencoded())
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
